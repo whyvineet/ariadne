79e53f7c546c7c043605db05b1b56ef8ecedc4d4
Vineet Kumar <vineetkumar17112004@gmail.com>
2026-06-24 01:17:07 +0530

    feat: add initial models and controllers for Board, BoardList, and Card resources

---
+++ backend/app/Http/Controllers/BoardListController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BoardList;
+use Illuminate\Http\Request;
+
+class BoardListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BoardList $boardList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BoardList $boardList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BoardList $boardList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BoardList $boardList)
+    {
+        //
+    }
+}
